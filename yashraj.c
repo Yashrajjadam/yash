1ce91c750f297ebbc079793bf63c33d0af57c51f
Yashrajjadam <yashrajsingh20122003@gmail.com>
2024-07-30 15:19:01 +0530

    first code

---
+++ yashraj.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double num1, num2, result;
+
+
+    printf("Select an operation (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    printf("Enter the first number: ");
+    scanf("%lf", &num1);
+    printf("Enter the second number: ");
+    scanf("%lf", &num2);
+
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '/':
+
+            if (num2 != 0) {
+                result = num1 / num2;
+                printf("Result: %.2lf\n", result);
+            } else {
+                printf("Error: Division by zero is not allowed.\n");
+            }
+            break;
+        default:
+            printf("Error: Invalid operator.\n");
+            break;
+    }
+
+    return 0;
+}
